0134b8490e962b7d98beaa64723e411d44718f42
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-07-09 14:31:30 -0400

    Created Limits bean and get controller .

---
+++ src/main/java/com/nephew/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,38 @@
+package com.nephew.microservices.limitsservice.bean;
+
+public class Limits {
+	private Integer minimum;
+	private Integer maximum;
+
+	public Integer getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(Integer minimum) {
+		this.minimum = minimum;
+	}
+
+	public Integer getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(Integer maximum) {
+		this.maximum = maximum;
+	}
+
+	public Limits(Integer minimum, Integer maximum) {
+		super();
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+	public Limits() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Limits [minimum=" + minimum + ", maximum=" + maximum + "]";
+	}
+
+}
